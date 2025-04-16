99f93bc4f4ec1c776dd2ab1d7bb62dcf11437cbe
sasanak71-old <mark.adams1971@yahoo.com>
2025-04-16 15:14:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "705939cc-cd3b-433b-b9cd-ae87bf322aa4"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatemark"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
